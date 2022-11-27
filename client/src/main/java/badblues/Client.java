a76b278ccf8927e43a561e14acabf7b0af6b5987
badbadblues _ <borovikxv.a@gmail.com>
2022-11-27 13:17:15 +0700

    configured submodules

---
+++ client/src/main/java/badblues/Client.java
@@ -0,0 +1,10 @@
+package badblues.client;
+
+public class Client {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello woradl!");
+
+    }
+
+}
---
+++ server/src/main/java/badblues/Server.java
@@ -0,0 +1,32 @@
+package badblues.server;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server {
+
+    public static void main(String[] args) {
+        System.out.println("Hello woradl!");
+        // ServerSocket serverSocket;
+        // Socket socket;
+        // try {
+        //     serverSocket = new ServerSocket(4999);
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
+        // while() {
+        //     try {
+        //         assert serverSocket != null : "Not connected";
+        //         socket = serverSocket.accept();
+        //         System.out.println("CLient accepted!");
+        //     } catch (IOException e) {
+        //         e.printStackTrace();
+        //     }
+        // }
+    }
+
+}
