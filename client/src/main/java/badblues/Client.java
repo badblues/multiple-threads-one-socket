86c320fb8e6af6ea3d1638c30334f5232140606c
badbadblues _ <borovikxv.a@gmail.com>
2022-11-27 20:30:26 +0700

    working version

--- client/src/main/java/badblues/Client.java
+++ client/src/main/java/badblues/Client.java
@@ -2,26 +2,40 @@ package badblues.client;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Vector;
+import java.util.LinkedList;
+import javafx.util.Pair;
+import java.lang.Exception;
 
 
 public class Client {
     
     private static Socket socket;
-    private static DataInputStream dataInputStream;
-    private static DataOutputStream dataOutputStream;
+    private static ObjectInputStream oin;
+    private static ObjectOutputStream oout;
+    private static Vector<ClientThread> threadVector = new Vector();
+    private static Queue<Pair<Integer, String>> queue = new LinkedList<>();
 
     public static void main(String[] args) {
         try {
             socket = new Socket("localhost", 4999);
-            dataInputStream = new DataInputStream(socket.getInputStream());
-            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            oout = new ObjectOutputStream(socket.getOutputStream());
+            oin = new ObjectInputStream(socket.getInputStream());
         } catch(IOException e) {
             e.printStackTrace();
         }
+        System.out.println("connected and made oin, oout");
         RecieveThread recieveThread = new RecieveThread();
         SendThread sendThread = new SendThread();
-        recieveThread.run();
-        sendThread.run();
+        recieveThread.start();
+        sendThread.start();
+        for (int i = 0; i < 10; i++) {
+            ClientThread thread = new ClientThread(i);
+            thread.start();
+            threadVector.add(thread);
+        }
     }
 
     private static class RecieveThread extends Thread {
@@ -33,6 +47,22 @@ public class Client {
         @Override
         public void run() {
             System.out.println("HELLO FROM RECIEVE THREAD");
+            while(true) {
+                try {
+                   Pair<Integer, String> pair = (Pair<Integer,String>)oin.readObject();
+                   System.out.println("got something");
+                   for (ClientThread thread : threadVector) {
+                       if (thread.getThreadId() == pair.getKey()) {
+                           thread.setPrintMode(true);
+                           thread.setMessage(pair.getValue());
+                           thread.unpause();
+                           break;
+                       }
+                   }
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
     } 
@@ -45,8 +75,91 @@ public class Client {
         @Override
         public void run() {
             System.out.println("HELLO FROM SEND THREAD");
+            while (true) {
+               try {
+                    synchronized(queue) {
+                        if (!queue.isEmpty()) {
+                            System.out.println("message: " + queue.peek());
+                            oout.writeObject(queue.poll());
+                        }
+                    }
+                } catch(IOException e) {
+                    e.printStackTrace();
+                } 
+            }   
         }
     }
 
+    private static class ClientThread extends Thread {
 
+        int id;
+        String message;
+        boolean printMode = false;
+
+        protected volatile boolean paused = false;
+	    protected final Object pauseLock = new Object();
+
+        public ClientThread(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public void run() {
+            while(true) {
+                synchronized (pauseLock) {
+                    if (paused) {
+                        try {
+                            pauseLock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            break;
+                        }
+                    }
+                }
+                if (printMode) {
+                    System.out.println(message);
+                    break;
+                }
+                generateWord(5);
+                Pair<Integer, String> pair = new Pair(id, message);
+                synchronized(queue) {
+                    queue.add(pair);
+                }
+                pause();
+            }
+        }
+
+        public void generateWord(int length) {
+            Random random = new Random();
+            char[] word = new char[length];
+            for(int j = 0; j < length; j++) {
+                word[j] = (char)('a' + random.nextInt(26));
+            }
+            message = new String(word);
+        }
+
+        public void pause() {
+            paused = true;
+        }
+
+        public void unpause() {
+            synchronized (pauseLock) {
+                paused = false;
+                pauseLock.notifyAll();
+            }
+        }
+
+        public int getThreadId() {
+            return id;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public void setPrintMode(boolean flag) {
+            this.printMode = flag;
+        }
+
+    }
 }
--- server/src/main/java/badblues/Server.java
+++ server/src/main/java/badblues/Server.java
@@ -1,34 +1,46 @@
 package badblues.server;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
+import java.util.Vector;
+import javafx.util.Pair;
+import java.util.concurrent.TimeUnit;
+import java.util.Random;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.lang.Exception;
+
+//TODO: solve dependency with javafx.controls
 
 public class Server {
+
+    private static ObjectInputStream oin;
+    private static ObjectOutputStream oout;
+    private static Queue<Pair<Integer, String>> queue = new LinkedList<>();
+    private static Vector<ServerThread> threadVector = new Vector();
 
     public static void main(String[] args) {
         ServerSocket serverSocket = null;
         Socket socket;
         try {
             serverSocket = new ServerSocket(4999);
+            assert serverSocket != null : "Not connected";
+            socket = serverSocket.accept();
+            System.out.println("CLient accepted!");
+            oout = new ObjectOutputStream(socket.getOutputStream());
+            oin = new ObjectInputStream(socket.getInputStream());
+            System.out.println("made oin, oout");
+            RecieveThread recieveThread = new RecieveThread();
+            SendThread sendThread = new SendThread();
+            recieveThread.start();
+            sendThread.start();
+            for (int i = 0; i < 10; i++) {
+                ServerThread thread = new ServerThread(i);
+                threadVector.add(thread);
+        }
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        while(true) {
-            try {
-                assert serverSocket != null : "Not connected";
-                socket = serverSocket.accept();
-                System.out.println("CLient accepted!");
-                RecieveThread recieveThread = new RecieveThread();
-                SendThread sendThread = new SendThread();
-                recieveThread.run();
-                sendThread.run();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 
@@ -41,9 +53,20 @@ public class Server {
         @Override
         public void run() {
             System.out.println("HELLO FROM RECIEVE THREAD");
-            if (din.available() > 0) {
-                    int i = din.readInt();
+            while(true) {
+                try {
+                    Pair<Integer, String> pair = (Pair<Integer,String>)oin.readObject();
+                    for (ServerThread thread : threadVector) {
+                       if (thread.getThreadId() == pair.getKey()) {
+                           thread.setPair(pair);
+                           thread.start();
+                           break;
+                       }
+                   }
+                } catch(Exception e) {
+                    e.printStackTrace();
                 }
+            }
         }
 
     } 
@@ -55,6 +78,63 @@ public class Server {
         @Override
         public void run() {
             System.out.println("HELLO FROM SEND THREAD");
+            while(true){
+                try {
+                    synchronized(queue) {
+                        if (!queue.isEmpty()) {
+                            System.out.println("message: " + queue.peek());
+                            oout.writeObject(queue.poll());
+                        }
+                    }
+                } catch(IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static class ServerThread extends Thread {
+
+        int id;
+        Pair<Integer, String> pair;
+
+        public ServerThread(int id) {
+            this.id = id;
+        } 
+
+        @Override
+        public void run() {
+            System.out.println("HELLO FROM SERVER THREAD" + id);
+            String newWord = reverseWord(pair.getValue());
+            pair = new Pair<Integer, String>(id, newWord);
+            synchronized(queue) {
+                queue.add(pair);
+            }
+        }
+
+        private String reverseWord(String word) {
+            System.out.println("GONNA WAIT " + id);
+            try {
+                Random random = new Random();
+                TimeUnit.MILLISECONDS.sleep(random.nextInt() % 5000);
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+            String newWord = "";
+            char ch;
+            for (int i=0; i<word.length(); i++) {
+                ch = word.charAt(i);
+                newWord = ch + newWord;
+            }
+            return newWord;
+        }
+
+        public int getThreadId() {
+            return id;
+        }
+
+        public void setPair(Pair<Integer, String> pair) {
+            this.pair = pair;
         }
     }
 
