a719e7d4c4fbd112e0b456c5216de6546f37eadc
badbadblues _ <borovikxv.a@gmail.com>
2022-11-27 15:11:03 +0700

    made recieving/sending threads

--- client/src/main/java/badblues/Client.java
+++ client/src/main/java/badblues/Client.java
@@ -1,19 +1,52 @@
 package badblues.client;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 
 public class Client {
     
     private static Socket socket;
+    private static DataInputStream dataInputStream;
+    private static DataOutputStream dataOutputStream;
 
     public static void main(String[] args) {
         try {
             socket = new Socket("localhost", 4999);
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
         } catch(IOException e) {
             e.printStackTrace();
         }
+        RecieveThread recieveThread = new RecieveThread();
+        SendThread sendThread = new SendThread();
+        recieveThread.run();
+        sendThread.run();
     }
+
+    private static class RecieveThread extends Thread {
+
+        //Поток приема ответов определяет по номеру в сообщении
+        //поток, передавший запрос и пробуждает его. Ответное сообщени
+        //находится в запросе, переданном потоком и выводится им
+
+        @Override
+        public void run() {
+            System.out.println("HELLO FROM RECIEVE THREAD");
+        }
+
+    } 
+
+    private static class SendThread extends Thread {
+
+        //Поток передачи от клиента выбирает сообщения из
+        //очереди и передает в соединение
+
+        @Override
+        public void run() {
+            System.out.println("HELLO FROM SEND THREAD");
+        }
+    }
+
 
 }
--- server/src/main/java/badblues/Server.java
+++ server/src/main/java/badblues/Server.java
@@ -22,9 +22,39 @@ public class Server {
                 assert serverSocket != null : "Not connected";
                 socket = serverSocket.accept();
                 System.out.println("CLient accepted!");
+                RecieveThread recieveThread = new RecieveThread();
+                SendThread sendThread = new SendThread();
+                recieveThread.run();
+                sendThread.run();
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private static class RecieveThread extends Thread {
+
+        //поток приема на сервере, приняв очередное сообщение, запускает поток
+        //исполнения запроса, по завершении которого в очередь ответов ставить ответ
+        //в котором сохраняется порядковый номер запроса
+
+        @Override
+        public void run() {
+            System.out.println("HELLO FROM RECIEVE THREAD");
+            if (din.available() > 0) {
+                    int i = din.readInt();
+                }
+        }
+
+    } 
+
+    private static class SendThread extends Thread {
+
+        //поток передачи ответов на сервере выбирает сообщения из очереди и передает в соединение
+
+        @Override
+        public void run() {
+            System.out.println("HELLO FROM SEND THREAD");
         }
     }
 
